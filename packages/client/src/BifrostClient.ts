5c421ecd4267ee09a199e556d29f3a15c0e1940b
DnsZhou <dns.t.zhou@gmail.com>
2021-03-02 00:53:16 +0000

    [NCL-6414]Test and Fix Web master-devel with HTTPS

--- packages/client/src/BifrostClient.ts
+++ packages/client/src/BifrostClient.ts
@@ -19,11 +19,11 @@ export class BifrostClient {
     constructor(host: string, verbose: boolean = false) {
         this.host = host;
 
-        this.rpcClient = new BifrostJsonRpcClient(`ws://${this.host}/socket`, verbose);
+        this.rpcClient = new BifrostJsonRpcClient(`wss://${this.host}/socket`, verbose);
 
         this.rpcClient.onMessage(result => this.onReceiveResult(result));
 
-        this.restClient = new BifrostRestClient(`http://${this.host}/`);
+        this.restClient = new BifrostRestClient(`https://${this.host}/`);
     }
 
     public close(): Promise<CloseEvent> {
