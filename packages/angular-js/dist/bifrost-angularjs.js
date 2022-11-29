47cc0714385e6cb7f541a3b7546689ab7a4e19c0
Patrik Korytar <patrik.korytar@gmail.com>
2022-11-29 15:09:34 +0100

    Update NPM (9.1.x) environment

--- packages/angular-js/dist/bifrost-angularjs.js
+++ packages/angular-js/dist/bifrost-angularjs.js
@@ -1,4 +1,4 @@
-/* bifrost-angularjs [v0.1.4] Fri Nov 18 2022*/
+/* bifrost-angularjs [v0.1.4] Tue Nov 29 2022*/
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
